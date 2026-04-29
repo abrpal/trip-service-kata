4eeb2c68269d36a82acc2eb55c8d0fcbedf99195
Abraham <a.palomino.gz@gmail.com>
2026-04-28 18:15:01 -0600

    added most relevant test cases and refactored TripService class.

--- TypeScript/src/trip/TripService.ts
+++ TypeScript/src/trip/TripService.ts
@@ -6,25 +6,24 @@ import TripDAO from "./TripDAO";
 
 export default class TripService {
     public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
-        const loggedUser: User = UserSession.getLoggedUser();
-        let isFriend: boolean = false;
-
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-
-            return tripList;
-        } else {
+        const loggedUser: User | null = this.getLoggedUser();
+        
+        if (loggedUser === null) {
             throw new UserNotLoggedInException();
         }
+
+        if (user.getFriends().includes(loggedUser)) {
+            return this.findTripsByUser(user);
+        }
+
+        return [];
+    }
+    
+    protected findTripsByUser(user: User): Trip[] {
+        return TripDAO.findTripsByUser(user);
+    }
+    
+    protected getLoggedUser(): User | null {
+        return UserSession.getLoggedUser();
     }
 }
--- TypeScript/test/TripServiceShould.test.ts
+++ TypeScript/test/TripServiceShould.test.ts
@@ -1,6 +1,65 @@
+import TripService from '../src/trip/TripService';
+import UserNotLoggedInException from '../src/exception/UserNotLoggedInException';
+import User from '../src/user/User';
+import Trip from '../src/trip/Trip';
 
-describe("TripServiceShould", () => {
-    it("adds numbers", () => {
-        expect(4 + 4).toBe(8);
+
+describe('User Not Logged In Exception', () => {
+    
+    class TestableTripService extends TripService {
+        protected getLoggedUser(): User | null {
+            return null;
+        }
+    }
+    
+    it('should throw UserNotLoggedInException', () => {
+        const testTripService = new TestableTripService();
+        const anyUser = new User();
+        
+        expect(() => testTripService.getTripsByUser(anyUser)).toThrow(UserNotLoggedInException);
+    });
+});
+
+
+describe('User Logged In, Is Friend', () => {
+    it('should return trips', () => {
+        
+        const loggedUser = new User();
+        const anyUser = new User();
+        
+        class TestableTripService extends TripService {
+            protected getLoggedUser(): User | null {
+                return loggedUser;
+            }
+            
+            protected findTripsByUser(user: User): Trip[] {
+                return [new Trip()];
+            }
+        }
+        
+        const testTripService = new TestableTripService();
+        
+        anyUser.addFriend(loggedUser);
+        
+        const trips = testTripService.getTripsByUser(anyUser);
+        expect(trips).toEqual([new Trip()]);
+    });
+});
+
+describe('User Logged In, Is not Friend', () => {
+    it('should return no trips', () => {
+        const loggedUser = new User();
+        
+        class TestableTripService extends TripService {
+            protected getLoggedUser(): User | null {
+                return loggedUser;
+            }
+        }
+        
+        const testTripService = new TestableTripService();
+        const anyUser = new User();
+        
+        const trips = testTripService.getTripsByUser(anyUser);
+        expect(trips).toEqual([]);
     });
 });
